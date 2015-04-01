c58bca1fbb260c00c2322dedc13ca28edb63f4ad
Sveinn Fannar Kristjánsson <sveinnfannar@gmail.com>
2015-04-01 23:39:57 +0000

    Some experimenting

--- app.js
+++ app.js
@@ -2,14 +2,29 @@
 
 var logfmt = require('logfmt');
 var express = require('express');
+var through = require('through');
 var app = express();
-app.use(logfmt.bodyParser());
+//app.use(logfmt.bodyParserStream()); // Parses content-type 'application/logplex-1'
 
 app.post('/', function (req, res) {
-  console.log(req.headers);
-  console.log(req.body);
-  // req.body.split('\n').forEach(console.log);
-  res.status(200).end();
+  req.on('data', function (data) {
+    console.log(data.toString());
+  });
+  req.on('end', function () {
+    res.send('OK');
+  });
+  return;
+  if(req.body === undefined) {
+    return res.send('OK');
+  }
+
+  req.body.pipe(through(processLine));
+
+  res.send('OK');
 });
 
 app.listen(process.env.PORT);
+
+function processLine (line) {
+  console.log(line);
+}
