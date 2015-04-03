51780fe167b6fcaa61575c6167a1bde9068c992e
Sveinn Fannar Kristjánsson <sveinnfannar@gmail.com>
2015-04-03 01:32:16 +0000

    Basic auth support and auth specific default tags

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
 'use strict';
 
-var _ = require('lodash');
-var logfmt = require('logfmt');
-var express = require('express');
-var through = require('through');
-var urlUtil = require('url');
-var StatsD = require('node-statsd');
-var statsd = new StatsD(parseStatsdUrl(process.env.STATSD_URL));
-var app = module.exports = express();
+let _ = require('lodash');
+let assert = require('assert');
+let logfmt = require('logfmt');
+let express = require('express');
+let through = require('through');
+let urlUtil = require('url');
+let StatsD = require('node-statsd');
+let basicAuth = require('basic-auth');
+let statsd = new StatsD(parseStatsdUrl(process.env.STATSD_URL));
+let app = module.exports = express();
 app.use(logfmt.bodyParserStream());
 
-
-/**
-  * Scratch Pad
-  * 1. Get some example data
-  * 2. Set up tests that emulate Heroku Logplex
-  * 3. Parse router metrics
-  * 4. Parse dyno metrics
-  * 5. Parse postgres metrics
-  * 6. Hardcode prefix & tags per heroku app id
-  * 7. Set up basic-auth (hardcode prefixes on auth instead?)
-  * [8. Read prefix & tags from logs]
-*/
+let apps = loadAppsFromEnv();
+console.log(apps);
 
 /**
  * Express app
  */
+
+app.use(function authenticate (req, res, next) {
+  let auth = basicAuth(req) || {};
+  let app = apps[auth.name];
+  if (app !== undefined && app.password === auth.pass) {
+    req.defaultTags = app.tags;
+    next();
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+});
+
 app.post('/', function (req, res) {
   if(req.body !== undefined) {
-    req.body.pipe(through(processLine));
+    req.body.pipe(through(line => processLine(line, req.defaultTags)));
   }
 
   res.send('OK');
 });
 
-var port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Server listening on port ' + port);
 });
@@ -45,18 +49,17 @@ app.listen(port, function () {
  * {
  *  <rule name>: {
  *    predicate: fn(line) -> bool
- *    process: fn(line) -> undefined
+ *    process: fn(line)
  *  }, ..
  * }
  */
-var rules = {
-  dynoRuntimeMetrics: {
-    predicate: function (line) {
-      return hasKeys(line, ['heroku', 'source', 'dyno']);
-    },
-    process: function (line) {
-      var tags = tagsToArr({ dyno: line.source });
-      var metrics = _.pick(line, (_, key) => key.startsWith('sample#'));
+let rules = {
+  dyno: {
+    predicate: line => hasKeys(line, ['heroku', 'source', 'dyno']),
+    process: function (line, defaultTags) {
+      let tags = tagsToArr({ dyno: line.source });
+      tags = _.union(tags, defaultTags);
+      let metrics = _.pick(line, (_, key) => key.startsWith('sample#'));
       _.forEach(metrics, function (value, key) {
         key = key.split('#')[1];
         key = key.replace(/_/g, '.');
@@ -65,12 +68,11 @@ var rules = {
     }
   },
 
-  routerResponseMetrics: {
-    predicate: function (line) {
-      return hasKeys(line, ['heroku', 'router', 'path', 'method', 'dyno', 'status', 'connect', 'service', 'at']);
-    },
-    process: function (line) {
-      var tags = tagsToArr(_.pick(line, ['dyno', 'method', 'status', 'path', 'host', 'code', 'desc', 'at']));
+  router: {
+    predicate: line => hasKeys(line, ['heroku', 'router', 'path', 'method', 'dyno', 'status', 'connect', 'service', 'at']),
+    process: function (line, defaultTags) {
+      let tags = tagsToArr(_.pick(line, ['dyno', 'method', 'status', 'path', 'host', 'code', 'desc', 'at']));
+      tags = _.union(tags, defaultTags);
       statsd.histogram('heroku.router.request.connect', line.connect, tags);
       statsd.histogram('heroku.router.request.service', line.service, tags);
       if (line.at === 'error') {
@@ -79,13 +81,12 @@ var rules = {
     }
   },
 
-  postgresMetrics: {
-    predicate: function (line) {
-      return hasKeys(line, ['source', 'heroku-postgres']);
-    },
-    process: function (line) {
-      var tags = tagsToArr({ source: line.source });
-      var metrics = _.pick(line, (_, key) => key.startsWith('sample#'));
+  postgres: {
+    predicate: line => hasKeys(line, ['source', 'heroku-postgres']),
+    process: function (line, defaultTags) {
+      let tags = tagsToArr({ source: line.source });
+      tags = _.union(tags, defaultTags);
+      let metrics = _.pick(line, (_, key) => key.startsWith('sample#'));
       _.forEach(metrics, function (value, key) {
         key = key.split('#')[1];
         statsd.histogram('heroku.postgres.' + key, value, tags);
@@ -99,12 +100,10 @@ var rules = {
  * Matches a line against a rule and processes it
  * @param {object} line
  */
-function processLine (line) {
-  console.log('Processing line:', line);
+function processLine (line, defaultTags) {
   _.forEach(rules, function (rule, name) {
     if (rule.predicate(line)) {
-      console.log('Line matches %s', name);
-      rule.process(line);
+      rule.process(line, defaultTags);
       return true; // Line is processed, we don't have to keep on matching
     }
   });
@@ -142,3 +141,23 @@ function tagsToArr (tags) {
 function hasKeys (object, keys) {
   return _.every(keys, _.partial(_.has, object));
 }
+
+
+/**
+ * Construct an apps object from the environment vars containing
+ * names, passwords and default tags for apps that may use the drain
+ */
+function loadAppsFromEnv () {
+  assert(process.env.ALLOWED_APPS);
+  let appNames = process.env.ALLOWED_APPS.split(',');
+  let apps = appNames.map(function (name) {
+    var nameUpper = name.toUpperCase();
+    assert(process.env[nameUpper + '_PASSWORD']);
+    return [name, {
+      password: process.env[nameUpper + '_PASSWORD'],
+      tags: (process.env[nameUpper + '_TAGS'] || '').split(',')
+    }];
+  });
+  return _.object(apps);
+}
+
---
+++ app.test.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var request = require('supertest-as-promised');
+var StatsD = require('node-statsd');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+// Set up environment vars
+process.env['ALLOWED_APPS'] = 'test-app';
+process.env['TEST-APP_PASSWORD'] = 'test-pass';
+process.env['TEST-APP_TAGS'] = 'default:tag';
+var app = require('./app');
+
+
+describe('Heroku Datadog Drain', function () {
+  beforeEach(function () {
+    sinon.spy(StatsD.prototype, 'histogram');
+    sinon.spy(StatsD.prototype, 'increment');
+  });
+
+  afterEach(function () {
+    StatsD.prototype.histogram.restore();
+    StatsD.prototype.increment.restore();
+  });
+
+  it('returns HTTP 401 if credentials are not found in environment', function () {
+    return request(app)
+      .post('/')
+      .auth('invalid-user', 'invalid-pass')
+      .expect(401);
+  });
+
+  it('sends router response metrics and error count to statsd', function () {
+    return request(app)
+    .post('/')
+    .auth('test-app', 'test-pass')
+    .set('Content-type', 'application/logplex-1')
+    .send('255 <158>1 2015-04-02T11:52:34.520012+00:00 host heroku router - at=info method=POST path="/users" host=myapp.com request_id=c1806361-2081-42e7-a8aa-92b6808eac8e fwd="24.76.242.18" dyno=web.1 connect=1ms service=37ms status=201 bytes=828\n' +
+          '293 <158>1 2015-04-02T11:52:37.888674+00:00 host heroku router - at=info method=GET path="/users/me/tasks" host=myapp.com request_id=d66b4e46-049f-4592-b0b0-a253dc1b0c62 fwd="24.77.243.44" dyno=web.2 connect=1ms service=64ms status=200 bytes=54414\n' +
+         '255 <158>1 2015-04-02T12:52:31.520012+00:00 host heroku router - at=error code=H12 desc="Request timeout" method=GET path="/" host=myapp.com fwd=17.17.17.17 dyno=web.1 connect=6ms service=30001ms status=503 bytes=0')
+    .expect(200)
+    .expect('OK')
+    .then(function () {
+      expect(StatsD.prototype.histogram.args).to.deep.equal([
+        ['heroku.router.request.connect', '1ms', ['dyno:web.1', 'method:POST', 'status:201', 'path:/users', 'host:myapp.com', 'at:info', 'default:tag']],
+        ['heroku.router.request.service', '37ms', ['dyno:web.1', 'method:POST', 'status:201', 'path:/users', 'host:myapp.com', 'at:info', 'default:tag']],
+        ['heroku.router.request.connect', '1ms', ['dyno:web.2', 'method:GET', 'status:200', 'path:/users/me/tasks', 'host:myapp.com', 'at:info', 'default:tag']],
+        ['heroku.router.request.service', '64ms', ['dyno:web.2', 'method:GET', 'status:200', 'path:/users/me/tasks', 'host:myapp.com', 'at:info', 'default:tag']],
+        ['heroku.router.request.connect', '6ms', ['dyno:web.1', 'method:GET', 'status:503', 'path:/', 'host:myapp.com', 'code:H12', 'desc:Request timeout', 'at:error', 'default:tag']],
+        ['heroku.router.request.service', '30001ms', ['dyno:web.1', 'method:GET', 'status:503', 'path:/', 'host:myapp.com', 'code:H12', 'desc:Request timeout', 'at:error', 'default:tag']],
+      ]);
+      expect(StatsD.prototype.increment.args).to.deep.equal([
+        ['heroku.router.error', 1, ['dyno:web.1', 'method:GET', 'status:503', 'path:/', 'host:myapp.com', 'code:H12', 'desc:Request timeout', 'at:error', 'default:tag']],
+      ]);
+    });
+  });
+
+  it('sends dyno runtime metrics to statsd', function () {
+    return request(app)
+    .post('/')
+    .auth('test-app', 'test-pass')
+    .set('Content-type', 'application/logplex-1')
+    .send('229 <45>1 2015-04-02T11:48:16.839257+00:00 host heroku web.1 - source=web.1 dyno=heroku.35930502.b9de5fce-44b7-4287-99a7-504519070cba sample#load_avg_1m=0.01 sample#load_avg_5m=0.02 sample#load_avg_15m=0.03\n' +
+          '329 <45>1 2015-04-02T11:48:16.839348+00:00 host heroku web.1 - source=web.1 dyno=heroku.35930502.b9de5fce-44b7-4287-99a7-504519070cba sample#memory_total=103.50MB sample#memory_rss=94.70MB sample#memory_cache=0.32MB sample#memory_swap=8.48MB sample#memory_pgpgin=36091pages sample#memory_pgpgout=11765pages')
+    .expect(200)
+    .expect('OK')
+    .then(function () {
+      expect(StatsD.prototype.histogram.args).to.deep.equal([
+        ['heroku.dyno.load.avg.1m', '0.01', ['dyno:web.1', 'default:tag']],
+        ['heroku.dyno.load.avg.5m', '0.02', ['dyno:web.1', 'default:tag']],
+        ['heroku.dyno.load.avg.15m', '0.03', ['dyno:web.1', 'default:tag']],
+        ['heroku.dyno.memory.total', '103.50MB', ['dyno:web.1', 'default:tag']],
+        ['heroku.dyno.memory.rss', '94.70MB', ['dyno:web.1', 'default:tag']],
+        ['heroku.dyno.memory.cache', '0.32MB', ['dyno:web.1', 'default:tag']],
+        ['heroku.dyno.memory.swap', '8.48MB', ['dyno:web.1', 'default:tag']],
+        ['heroku.dyno.memory.pgpgin', '36091pages', ['dyno:web.1', 'default:tag']],
+        ['heroku.dyno.memory.pgpgout', '11765pages', ['dyno:web.1', 'default:tag']]
+      ]);
+    });
+  });
+  
+  it('sends PostgreSQL metrics to statsd', function () {
+    return request(app)
+    .post('/')
+    .auth('test-app', 'test-pass')
+    .set('Content-type', 'application/logplex-1')
+    .send('542 <134>1 2015-04-02T11:47:55+00:00 host app heroku-postgres - source=HEROKU_POSTGRESQL_TEAL sample#current_transaction=6709 sample#db_size=18032824bytes sample#tables=16 sample#active-connections=4 sample#waiting-connections=0 sample#index-cache-hit-rate=0.99971 sample#table-cache-hit-rate=0.99892 sample#load-avg-1m=0.315 sample#load-avg-5m=0.22 sample#load-avg-15m=0.225 sample#read-iops=25.996 sample#write-iops=1.629 sample#memory-total=15666128kB sample#memory-free=233092kB sample#memory-cached=14836812kB sample#memory-postgres=170376kB')
+    .expect(200)
+    .expect('OK')
+    .then(function () {
+      expect(StatsD.prototype.histogram.args).to.deep.equal([
+        ['heroku.postgres.current_transaction', '6709', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.db_size', '18032824bytes', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.tables', '16', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.active-connections', '4', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.waiting-connections', '0', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.index-cache-hit-rate', '0.99971', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.table-cache-hit-rate', '0.99892', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.load-avg-1m', '0.315', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.load-avg-5m', '0.22', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.load-avg-15m', '0.225', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.read-iops', '25.996', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.write-iops', '1.629', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.memory-total', '15666128kB', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.memory-free', '233092kB', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.memory-cached', '14836812kB', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']],
+        ['heroku.postgres.memory-postgres', '170376kB', ['source:HEROKU_POSTGRESQL_TEAL', 'default:tag']]
+      ]);
+    });
+  });
+});
